add8988b169035352df96607e96b1a7b1ac17b0d
BedinovOleksandr <49485882+BedinovOleksandr@users.noreply.github.com>
2019-06-10 11:54:11 +0300

    test

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import telebot
 
-
 mySlaveToken = '877949812:AAF1memBG6qplUprQZ3nmGORHtYWIdWQ7oQ'
 #
 bot = telebot.TeleBot(mySlaveToken)
@@ -8,8 +7,11 @@ bot = telebot.TeleBot(mySlaveToken)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    print('userId: ', message.chat.id, ' Имя: ', message.from_user.first_name)
-    bot.send_message(message.chat.id, "Я бот Александра. Привет {0}!".format(message.from_user.first_name))
+    print('userId: ', message.chat.id, ' Имя: ', message.from_user.first_name, message.from_user.last_name)
+    if message.text == '/start':
+        bot.send_message(message.chat.id, "Я бот Александра. Привет {0}! Напиши мне что то".format(message.from_user.first_name + ' ' + message.from_user.last_name ))
+    else:
+        bot.send_message(message.chat.id, "Я пока мало что умею но со временем я смогу больше. Хорошего Вам дня!")
 
 
 if __name__ == '__main__':
